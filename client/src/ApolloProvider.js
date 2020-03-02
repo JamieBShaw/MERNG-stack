4a68d2b2c519b00f1a4a527235f62a1bd26754ca
Jamie <jamiebshaw1095@gmail.com>
2020-03-02 21:11:19 +0000

    Got ApolloProvider running

---
+++ client/src/ApolloProvider.js
@@ -0,0 +1,21 @@
+import React from "react";
+import App from "./App";
+import ApolloClient from "apollo-client";
+import { inMemoryCache } from "apollo-cache-inmemory";
+import { createHttpLink } from "apollo-link-http";
+import { ApolloProvider } from "@apollo/react-hooks";
+
+const httpLink = createHttpLink({
+	uri: "http://localhost:5000"
+});
+
+const client = new ApolloClient({
+	link: httpLink,
+	cache: new inMemoryCache()
+});
+
+export default (
+	<ApolloProvider client={client}>
+		<App />
+	</ApolloProvider>
+);
