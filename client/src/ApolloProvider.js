8dafe6fcb8fcf6e9b94270457ae8157230719fac
Jamie <jamiebshaw1095@gmail.com>
2020-03-03 11:45:15 +0000

    MenuBar Routes setup

--- client/src/ApolloProvider.js
+++ client/src/ApolloProvider.js
@@ -1,9 +1,10 @@
 import React from "react";
 import App from "./App";
 import ApolloClient from "apollo-client";
-import { inMemoryCache } from "apollo-cache-inmemory";
-import { createHttpLink } from "apollo-link-http";
+
 import { ApolloProvider } from "@apollo/react-hooks";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { createHttpLink } from "apollo-link-http";
 
 const httpLink = createHttpLink({
 	uri: "http://localhost:5000"
@@ -11,7 +12,7 @@ const httpLink = createHttpLink({
 
 const client = new ApolloClient({
 	link: httpLink,
-	cache: new inMemoryCache()
+	cache: new InMemoryCache()
 });
 
 export default (
